a516ca0d6f7475625622a28a3a10ce76d9249007
JuliaLabre <juliarlabres@gmail.com>
2022-10-26 16:41:29 -0300

    inicio de artigos dinâmicos

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,47 @@
 <?php
 
-// Configura PHP para UTF-8:
-header('Content-Type: text/html; charset=utf-8');
+/**
+ * Coleção (array) com as opções configuração inicial do aplicativo:
+ * Referências:
+ *  • https://www.w3schools.com/php/php_arrays.asp
+ *  • https://www.w3schools.com/php/func_array.asp
+ **/
+$c = array(
+    'sitename' => 'Mulheres.Tech',
+    'siteslogan' => 'Programadoras do Futuro',
+    'sitelogo' => '<i class="fa-solid fa-laptop-code fa-fw"></i>',
+    'sitefavicon' => '/img/favicon.jpg',
+    'titlesep' => '&middot;&middot;&middot;'
+);
 
-// Define o fuso horário para "horário de Brasília":
-date_default_timezone_set('America/Sao_Paulo');
+/**
+ * Coleção com a lista de redes sociais:
+ * OBS: o item 'icon' faz referência ao nome do ícone na biblioteca 
+ * Font Awesome.
+ **/
+$s = array(
+    array(
+        'name' => 'Facebook',
+        'link' => 'https://facebook.com/Mulheres.Tech',
+        'icon' => 'fa-square-facebook'
+    ),
+    array(
+        'name' => 'Youtube',
+        'link' => 'https://youtube.com/Mulheres.Tech',
+        'icon' => 'fa-square-youtube'
+    ),
+    array(
+        'name' => 'GitHub',
+        'link' => 'https://github.com/Mulheres.Tech',
+        'icon' => 'fa-square-github'
+    )
+);
+
+/**
+ * Inicializa variáveis de uso geral:
+ * Todas as variáveis com valor "vazio".
+ **/
+$page_title = $page_content = $page_css = $page_js = '';
 
 // Dados de conexão com MySQL no XAMPP:
 $hostname = 'localhost';
@@ -12,7 +49,26 @@ $username = 'root';
 $password = '';
 $database = 'mulherestech';
 
-// Conexão com o MySQL e banco de dados:
+/**
+ * Configura PHP para UTF-8:
+ * Referências:
+ *  • https://www.w3schools.com/php/func_network_header.asp
+ **/
+header('Content-Type: text/html; charset=utf-8');
+
+/**
+ * Define o fuso horário para "horário de Brasília":
+ * Referências:
+ *  • https://www.w3schools.com/php/func_date_default_timezone_set.asp
+ **/
+date_default_timezone_set('America/Sao_Paulo');
+
+/**
+ * Conexão com o MySQL e banco de dados:
+ * Referências:
+ *  • https://www.w3schools.com/php/php_mysql_intro.asp
+ *    OBS: O link acima é a página inicial sibre a bilbioteca MySQLi.
+ **/
 $conn = new mysqli($hostname, $username, $password, $database);
 
 // Seta transações com MySQL/MariaDB para UTF-8:
